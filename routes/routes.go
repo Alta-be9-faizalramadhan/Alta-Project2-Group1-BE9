8cee13c6d37067a42e3e9372b5746ea5e2fd64e5
FaizalSundara <faizalramadhansundara@gmail.com>
2022-06-30 05:39:49 +0700

    add jwt middleware for fitures shoppingcartdetails

--- routes/routes.go
+++ routes/routes.go
@@ -37,10 +37,10 @@ func New(presenter factory.Presenter) *echo.Echo {
 
 	e.GET("orders/:id", presenter.ShoppingCartPresenter.GetAllHistoryOrder, middlewares.JWTMiddleware())
 
-	e.GET("orderdetails/:id", presenter.ShoppingCartDetailPresenter.GetAllCartDetails)
-	e.POST("orderdetails", presenter.ShoppingCartDetailPresenter.InsertCartDetails)
-	e.DELETE("orderdetails/:id", presenter.ShoppingCartDetailPresenter.DeleteCartDetails)
-	e.PUT("orderdetails/:id", presenter.ShoppingCartDetailPresenter.UpdateCartDetails)
+	e.GET("orderdetails/:id", presenter.ShoppingCartDetailPresenter.GetAllCartDetails, middlewares.JWTMiddleware())
+	e.POST("orderdetails", presenter.ShoppingCartDetailPresenter.InsertCartDetails, middlewares.JWTMiddleware())
+	e.DELETE("orderdetails/:id", presenter.ShoppingCartDetailPresenter.DeleteCartDetails, middlewares.JWTMiddleware())
+	e.PUT("orderdetails/:id", presenter.ShoppingCartDetailPresenter.UpdateCartDetails, middlewares.JWTMiddleware())
 
 	e.GET("categories", presenter.CategoryPresenter.GetAllCategory)
 	return e
